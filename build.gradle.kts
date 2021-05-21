abc31d8a3b72604b0d4fd8f1ef677f925fe3246c
Zak <shearmanzak@gmail.com>
2021-05-21 21:03:16 +0100

    Add configuration processing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     java
     id("org.springframework.boot") version "2.4.5"
-    id ("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.github.johnrengelman.shadow") version "7.0.0"
     id("io.freefair.lombok") version "5.3.0"
-    id ("org.asciidoctor.convert") version "1.5.8"
+    id("org.asciidoctor.convert") version "1.5.8"
 }
 
 group = "net.titanrealms.api"
@@ -29,7 +29,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation( "org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
     // github api client
     implementation("org.kohsuke:github-api:1.127")
@@ -38,16 +38,17 @@ dependencies {
     // HOCON files
     implementation("com.typesafe:config:1.4.1")
 
-    developmentOnly( "org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     //runtimeOnly ("io.micrometer:micrometer-registry-influx")
 
     // API Documentation
     annotationProcessor("io.swagger.core.v3:swagger-annotations:2.1.7")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     // Display API documentation
     developmentOnly("org.springdoc:springdoc-openapi-ui:1.5.2")
 
-    testImplementation( "org.springframework.boot:spring-boot-starter-test")
-    testImplementation ("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
 ext {
@@ -60,6 +61,9 @@ tasks {
     }
     val testTask = withType<Test> {
         outputs.dir(snippetsDir)
+    }
+    withType<JavaCompile> {
+        inputs.files(processResources)
     }
     withType<org.asciidoctor.gradle.AsciidoctorTask> {
         dependsOn(testTask)
