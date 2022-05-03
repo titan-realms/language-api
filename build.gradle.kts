9fb17731a80ec38afb82ac53371f48cc3dccc636
Zak <shearmanzak@gmail.com>
2022-05-03 01:19:07 +0100

    Remove some stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,13 @@
 plugins {
     java
-    id("org.springframework.boot") version "2.4.5"
+    id("org.springframework.boot") version "2.6.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.github.johnrengelman.shadow") version "7.0.0"
     id("io.freefair.lombok") version "5.3.0"
-    id("org.asciidoctor.convert") version "1.5.8"
 }
 
 group = "net.titanrealms.api"
 version = "0.0.1-SNAPSHOT"
-
-val snippetsDir by extra { file("build/generated-snippets") }
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-}
-
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
-}
 
 repositories {
     mavenCentral()
@@ -33,8 +20,6 @@ dependencies {
 
     // github api client
     implementation("org.kohsuke:github-api:1.127")
-    // lang formatting
-    implementation("net.titanrealms.lang.formatter:lang-formatter:1.0-SNAPSHOT")
     // HOCON files
     implementation("com.typesafe:config:1.4.1")
 
@@ -49,24 +34,4 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-}
-
-ext {
-    set("snippetsDir", snippetsDir)
-}
-
-tasks {
-    test {
-        useJUnitPlatform()
-    }
-    val testTask = withType<Test> {
-        outputs.dir(snippetsDir)
-    }
-    withType<JavaCompile> {
-        inputs.files(processResources)
-    }
-    withType<org.asciidoctor.gradle.AsciidoctorTask> {
-        dependsOn(testTask)
-        inputs.dir(snippetsDir)
-    }
 }
